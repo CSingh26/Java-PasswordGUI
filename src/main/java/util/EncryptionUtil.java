c66451316ad4d5ebf0f40ff78fd98c0042bace9f
Csingh26 <singh.chaittanya@gmail.com>
2024-02-17 10:52:24 +0530

    EncryptPasswdFunction

--- src/main/java/util/EncryptionUtil.java
+++ src/main/java/util/EncryptionUtil.java
@@ -1,5 +1,57 @@
 package main.java.util;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 
 public class EncryptionUtil {
+
+    private static final String ALGO = "AES/CBC/PKCS5Padding";
+
+    public static SecretKey generateKey(int n) throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(n);
+        SecretKey key = keyGenerator.generateKey();
+        
+        return key;
+    }
+
+    public static IvParameterSpec generateIv() {
+        byte[] iv = new byte[16];
+        new SecureRandom().nextBytes(iv);
+
+        return new IvParameterSpec(iv);
+    }
+
+    public static String encrypt(String input, SecretKey key,
+        IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException,
+        InvalidAlgorithmParameterException, InvalidKeyException,
+        BadPaddingException, IllegalBlockSizeException {
     
+            Cipher cipher = Cipher.getInstance(ALGO);
+            cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+            byte[] cipherText = cipher.doFinal(input.getBytes());
+            return Base64.getEncoder().encodeToString(cipherText);
+    }
+
+    public static String decrypt(String input, SecretKey key,
+        IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException,
+        InvalidAlgorithmParameterException, InvalidKeyException,
+        BadPaddingException, IllegalBlockSizeException {
+    
+            Cipher cipher = Cipher.getInstance(ALGO);
+            cipher.init(Cipher.DECRYPT_MODE, key, iv);
+            byte[] plainText = cipher.doFinal(Base64.getDecoder().decode(input));
+            return new String(plainText);
+    }
+
 }
--- src/main/java/util/PasswordGenerator.java
+++ src/main/java/util/PasswordGenerator.java
@@ -1,5 +1,72 @@
 package main.java.util;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
 
 public class PasswordGenerator {
-    
+
+    private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String DIGIT = "1234567890";
+    private static final String SPECIAL = "!@#$%^&*()_-+={}[]?/><.,~`";
+
+    private final boolean useUpper;
+    private final boolean useLower;
+    private final boolean useDigits;
+    private final boolean useSpecial;
+
+    private final SecureRandom randomPwd = new SecureRandom();
+
+    public PasswordGenerator(boolean useUpper, boolean useLower, boolean useDigits, boolean useSpecial) {
+        this.useUpper = useUpper;
+        this.useLower = useLower;
+        this.useDigits = useDigits;
+        this.useSpecial = useSpecial;
+    }
+
+    public String generate(int length) {
+        StringBuilder password = new StringBuilder(length);
+        List<String> charCat = new ArrayList<>(4);
+
+        if (useUpper) {
+            charCat.add(UPPERCASE);
+        }
+
+        if (useLower) {
+            charCat.add(LOWERCASE);
+        }
+
+        if (useDigits) {
+            charCat.add(DIGIT);
+        }
+
+        if (useSpecial) {
+            charCat.add(SPECIAL);
+        }
+        
+        for (String cat : charCat) {
+            password.append(cat.charAt(randomPwd.nextInt(cat.length())));
+        }
+
+        for (int i = password.length(); i < length; i++) {
+            String cat = charCat.get(randomPwd.nextInt(charCat.size()));
+            password.append(cat.charAt(randomPwd.nextInt(cat.length())));
+        }
+
+        List<Character> passwordChar = new ArrayList<>(password.length());
+        for (int i = 0; i < password.length(); i++) {
+            passwordChar.add(password.charAt(i));
+        }
+
+        Collections.shuffle(passwordChar);
+
+        StringBuilder finalPwd = new StringBuilder(length);
+        for (char c : passwordChar) {
+            finalPwd.append(c);
+        }
+
+        return finalPwd.toString();
+    }
+
 }
